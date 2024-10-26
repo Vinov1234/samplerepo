878f6a14f99e60bd844957a7f38ab68bbf76dacd
Vinov1234 <131349760+Vinov1234@users.noreply.github.com>
2024-10-26 07:34:06 -0700

    completed in 1st chapter

---
+++ chpater1/math opreators.py
@@ -0,0 +1,67 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2**3
+8
+22%8
+6
+22//8
+2
+
+22/8
+2.75
+3*5
+15
+5-2
+3
+2+3*6
+20
+(23)*6
+138
+(2+3)*6
+30
+2**8
+256
+23//7
+3
+>>> 23%7
+2
+>>> (5-1)*((7+1)/(3-1))
+16.0
+>>> 5+
+SyntaxError: invalid syntax
+>>> 42+5 +*2
+SyntaxError: invalid syntax
+>>> 'alice'+'bob'
+'alicebob'
+>>> 'alice'+42
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    'alice'+42
+TypeError: can only concatenate str (not "int") to str
+>>> 'alice'*5
+'alicealicealicealicealice'
+>>> 'alice'*'bob'
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    'alice'*'bob'
+TypeError: can't multiply sequence by non-int of type 'str'
+>>> spam = 40
+>>> spam
+40
+>>> eggs  =  2
+>>> eggs
+2
+>>> spam+eggs
+42
+>>> spam+eggs+spam
+82
+>>> spam= spam+2
+>>> spam
+42
+>>> str(0)
+
+
+
+
+
+
